170ea08542a5e2a0d48094e32a8bef22b83d1fdd
Sohidul Islam Ananto <anontom90@gmail.com>
2025-08-01 11:57:40 +0600

    information model

---
+++ src/app/modules/informations/information.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+enum INFORMATION_TYPE {
+    ABOUT_US = 'ABOUT_US',
+    PRIVACY_POLICY = 'PRIVACY_POLICY',
+    TERMS_OF_SERVICE = 'TERMS_OF_SERVICE',
+}
+
+interface Information {
+    type: INFORMATION_TYPE;
+    title: string;
+    content: string;
+}
+
+export const InformationModal = model<Information>('Information', new Schema<Information>({
+    type: {
+        type: String,
+        enum: Object.values(INFORMATION_TYPE),
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+}));
